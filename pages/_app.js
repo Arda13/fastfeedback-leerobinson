4ec286947d6e5ec9592fc665b10c97cb7a9d8e85
Arda Kaya <ardaka23@outlook.com>
2020-09-25 01:26:32 +0300

    end of 2nd vid

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,38 @@
-import '../styles/globals.css';
-import { AuthProvider } from '../lib/auth';
+import { Global, css } from '@emotion/core';
+import { AuthProvider } from '@/lib/auth';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import customTheme from '@/styles/theme';
 
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset></CSSReset>
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behaviour: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      ></Global>
+      {children}
+    </>
+  );
+};
 function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
 export default App;
-
-
