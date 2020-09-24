80775a3632bb980b2158baf464a3951a3e39801b
Arda Kaya <ardaka23@outlook.com>
2020-09-25 01:41:50 +0300

    end of 2nd vid (#6)
    * end of 1st vid
    
    * db added
    
    * git test
    
    * end of 2nd vid

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,26 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { auth } from 'firebase';
-import { useAuth } from '../lib/auth';
+import { Button, Heading, Text, Code } from '@chakra-ui/core';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
       </Head>
+      <main>
+        <Heading>Fast Feedback</Heading>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Fast Feedback</a>
-        </h1>
-
-        <p className={styles.description}>
-          <code>{auth.user ? auth.user.email : 'None'}</code>
-        </p>
-        <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-        <div>{auth.user?.email}</div>
-        {auth?.user && (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
+        <Text>
+          <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
