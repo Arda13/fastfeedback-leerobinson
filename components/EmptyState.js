6552fe3718b41c5b44736acba122335255c91fb7
Arda Kaya <ardaka23@outlook.com>
2020-09-26 23:48:16 +0300

    mid 4th vid (#8)
    * end of 1st vid
    
    * db added
    
    * git test
    
    * end of 2nd vid
    
    * end of 3rd vid
    
    * mid 4th vid

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Heading, Flex, Text, Button } from '@chakra-ui/core';
 
-import DashboardShell from './DashboardShell';
 import AddSiteModal from './AddSiteModal';
 
 const FreePlanEmptyState = () => (
-  <DashboardShell>
-    <Flex
-      width='100%'
-      backgroundColor='white'
-      borderRadius='8px'
-      p={16}
-      justify='center'
-      align='center'
-      direction='column'
-    >
-      <Heading size='lg' mb={2}>
-        You haven't added any sites.
-      </Heading>
-      <Text mb={4}>Welcome, Let's get started</Text>
-      <AddSiteModal></AddSiteModal>
-    </Flex>
-  </DashboardShell>
+  <Flex
+    width='100%'
+    backgroundColor='white'
+    borderRadius='8px'
+    p={16}
+    justify='center'
+    align='center'
+    direction='column'
+  >
+    <Heading size='lg' mb={2}>
+      You haven't added any sites.
+    </Heading>
+    <Text mb={4}>Welcome, Let's get started</Text>
+    <AddSiteModal>Add your First Site</AddSiteModal>
+  </Flex>
+
 );
 
 export default FreePlanEmptyState;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,15 +1,31 @@
 import Head from 'next/head';
+import useSWR from 'swr';
 import { Button, Heading, Text, Code, Icon, Box, Flex } from '@chakra-ui/core';
-import { useAuth } from '@/lib/auth';
 import EmptyState from '@/components/EmptyState';
+import fetcher from '@/utils/fetcher';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboardShell';
+import { useAuth } from '@/lib/auth';
+import SiteTable from '@/components/SiteTable';
 
 const Dashboard = () => {
   const auth = useAuth();
+  const { data } = useSWR('/api/sites', fetcher);
 
-  if (!auth.user) {
-    return 'Loading...';
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton></SiteTableSkeleton>
+      </DashboardShell>
+    );
   }
-  return <EmptyState></EmptyState>;
+
+  return (
+    <DashboardShell>
+      {data.site ? <SiteTable sites={data.sites} /> : <EmptyState></EmptyState>}
+    </DashboardShell>
+  );
+
 };
 
 export default Dashboard;
